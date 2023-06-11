db0fa2e14fff2cc1141571e7b83fcb368b3cb4d5
Niraj Patil <112927422+nirajpatil22400@users.noreply.github.com>
2023-06-11 19:51:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,30 @@
+// Your Script here.
+
 const lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-  "?": "?",
-  ",": ",",
+  'A': 'N','B': 'O','C': 'P','D': 'Q',
+  'E': 'R','F': 'S','G': 'T','H': 'U',
+  'I': 'V','J': 'W','K': 'X','L': 'Y',
+  'M': 'Z','N': 'A','O': 'B','P': 'C',
+  'Q': 'D','R': 'E','S': 'F','T': 'G',
+  'U': 'H','V': 'I','W': 'J','X': 'K',
+  'Y': 'L','Z': 'M', '?': '?', ',': ','
 };
 
-function rot13(encodedStr) {
-  let decodedArr = [];
-
-  for (let i = 0; i < encodedStr.length; i++) {
-    const character = encodedStr[i];
-    const decodedChar = lookup[character];
-
-    decodedArr.push(decodedChar || character);
-  }
-
-  return decodedArr.join("");
+function rot13(encodedStr){
+   let decodedArr = []; // Your Result goes here
+  // Only change code below this line
+	for(let i=0; i<encodedStr.length; i++){
+		if(lookup[encodedStr[i]] == undefined){
+			decodedArr.push(encodedStr[i]);
+		}
+		else decodedArr.push(lookup[encodedStr[i]]);
+	}
+return decodedArr;
+ //return decodedArr
 }
 
-console.log(rot13("SERR YBIR? NPPVBWBO")); // "FREE LOVE? ACCIOWAND"
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+
+// console.log(rot13("SERR YBIR? NPPVBWBO"));
+
+window.rot13 = rot13;
